a383963dda66a28cca213cbc9721be684fb9a27f
Danish Maknojia <danishmak@Danishs-MacBook-Pro.local>
2024-10-21 14:19:07 -0400

    Setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ETLpipeline",
+    version="0.1.0",
+    description="ETL and Query",
+    author="Danish Maknojia",
+    author_email="d.maknojia@duke.edu",
+    packages=find_packages(),  # Automatically find all packages and sub-packages
+    install_requires=[
+        "databricks-sql-connector",
+        "pandas",
+        "python-dotenv",
+    ],
+    entry_points={
+        "console_scripts": [
+            "etl_query=main:main",
+        ],
+    },
+)
